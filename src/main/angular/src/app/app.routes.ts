d289dcb9272ba467866c349372a972626b462546
Jason <extremewinds@gmail.com>
2025-08-14 23:22:16 +0200

    install botstrap font-awesome and add routing to components

--- src/main/angular/src/app/app.routes.ts
+++ src/main/angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+// routes.ts
+import { LayoutComponent } from './layout/layout/layout.component';
+import { AppComponent } from './app.component';
 import { Routes } from '@angular/router';
+import {MiscComponent} from './pages/misc/misc.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: AppComponent },
+      { path: 'misc', component: MiscComponent }
+    ]
+  }
+];
