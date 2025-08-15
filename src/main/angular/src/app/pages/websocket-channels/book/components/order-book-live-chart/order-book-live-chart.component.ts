fd264afa1294f7c256562599585227976b760e13
Jason <extremewinds@gmail.com>
2025-08-16 00:31:47 +0200

    reformat code

--- src/main/angular/src/app/pages/websocket-channels/book/components/order-book-live-chart/order-book-live-chart.component.ts
+++ src/main/angular/src/app/pages/websocket-channels/book/components/order-book-live-chart/order-book-live-chart.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HighchartsChartComponent } from 'highcharts-angular';
+import {Component, inject, OnInit} from '@angular/core';
+import {HighchartsChartComponent} from 'highcharts-angular';
 import * as Highcharts from 'highcharts';
-import { OrderbookData, OrderPoint } from '../../model/dto';
-import { BookWebsocketService } from '../../services/book.websocket.service';
+import {OrderbookData, OrderPoint} from '../../model/dto';
+import {BookWebsocketService} from '../../services/book.websocket.service';
 
 @Component({
   selector: 'app-order-book-live-chart',
@@ -26,20 +26,20 @@ export class OrderBookLiveChartComponent implements OnInit {
     // @ts-ignore
     instrumentName: this.instrumentName,
 
-      chart: {
-        animation: {
-          duration: 200
-        },
-        backgroundColor: '#23232f',
-        marginTop: 70,
+    chart: {
+      animation: {
+        duration: 200
       },
+      backgroundColor: '#23232f',
+      marginTop: 70,
+    },
 
-      title: {
-        text: 'Order book live chart',
-        style: {
-          color: '#ffffff'
-        }
-      },
+    title: {
+      text: 'Order book live chart',
+      style: {
+        color: '#ffffff'
+      }
+    },
 
     tooltip: {
       headerFormat: 'Price: <b>${point.point.price:,.5f}</b></br>',
@@ -47,157 +47,164 @@ export class OrderBookLiveChartComponent implements OnInit {
       shape: 'rect'
     },
 
-      xAxis: [{
-        reversed: true,
-        visible: false,
-        title: {
-          text: 'Market depth / price'
-        },
-        accessibility: {
-          description: 'Bid orders'
-        }
-      }, {
-        opposite: true,
-        visible: false,
-        title: {
-          text: 'Market depth / price'
-        },
-        accessibility: {
-          description: 'Ask orders'
-        }
-      }],
+    legend: {
+      itemStyle: {
+        color: '#ffffff',
+        fontSize: '14px'
+      }
+    },
 
-      yAxis: [{
-        offset: 0,
-        visible: true,
-        opposite: true,
-        gridLineWidth: 0,
-        tickAmount: 1,
-        left: '50%',
-        width: '50%',
-        title: {
-          text: 'Amount of ask orders',
-          style: {
-            visibility: 'hidden'
-          }
-        },
-        // min: 0,
-        // max: 100000,
-        labels: {
-          enabled: true,
-          formatter: function () {
-            if ((this as any).isLast) {
-              return 'Asks';
-            }
-            return '';
-          },
-          style: {
-            color: '#ffffff',
-            fontSize: '16px',
-            fontWeight: '700'
-          },
-          y: 10
-        }
-      }, {
-        offset: 0,
-        visible: true,
-        opposite: true,
-        gridLineWidth: 0,
-        tickAmount: 2,
-        left: '0%',
-        width: '50%',
-        reversed: true,
-        title: {
-          text: 'Amount of bid orders',
-          style: {
-            visibility: 'hidden'
-          }
-        },
-        // min: 0,
-        // max: 100000,
-        labels: {
-          enabled: true,
-          formatter: function () {
-            // "this" here is Highcharts axis label context, not your Angular component
-            const instrumentName = (this.axis.chart.userOptions as any).instrumentName || '';
-            if ((this as any).pos === 0) {
-              return `Price (${instrumentName})`;
-            }
-            if ((this as any).isLast) {
-              return 'Bids';
-            }
-            return '';
-          },
-          style: {
-            color: '#ffffff',
-            fontSize: '16px',
-            fontWeight: '700'
-          },
-          y: 10
-        }
-      }],
+    xAxis: [{
+      reversed: true,
+      visible: false,
+      title: {
+        text: 'Market depth / price'
+      },
+      accessibility: {
+        description: 'Bid orders'
+      }
+    }, {
+      opposite: true,
+      visible: false,
+      title: {
+        text: 'Market depth / price'
+      },
+      accessibility: {
+        description: 'Ask orders'
+      }
+    }],
 
-      plotOptions: {
-        series: {
-          animation: false,
-          dataLabels: {
-            enabled: true,
-            color: '#ffffff'
-          },
-          borderWidth: 0,
-          crisp: false
+    yAxis: [{
+      offset: 0,
+      visible: true,
+      opposite: true,
+      gridLineWidth: 0,
+      tickAmount: 1,
+      left: '50%',
+      width: '50%',
+      title: {
+        text: 'Amount of ask orders',
+        style: {
+          visibility: 'hidden'
         }
       },
+      // min: 0,
+      // max: 100000,
+      labels: {
+        enabled: true,
+        formatter: function () {
+          if ((this as any).isLast) {
+            return 'Asks';
+          }
+          return '';
+        },
+        style: {
+          color: '#ffffff',
+          fontSize: '16px',
+          fontWeight: '700'
+        },
+        y: 10
+      }
+    }, {
+      offset: 0,
+      visible: true,
+      opposite: true,
+      gridLineWidth: 0,
+      tickAmount: 2,
+      left: '0%',
+      width: '50%',
+      reversed: true,
+      title: {
+        text: 'Amount of bid orders',
+        style: {
+          visibility: 'hidden'
+        }
+      },
+      // min: 0,
+      // max: 100000,
+      labels: {
+        enabled: true,
+        formatter: function () {
+          // "this" here is Highcharts axis label context, not your Angular component
+          const instrumentName = (this.axis.chart.userOptions as any).instrumentName || '';
+          if ((this as any).pos === 0) {
+            return `Price (${instrumentName})`;
+          }
+          if ((this as any).isLast) {
+            return 'Bids';
+          }
+          return '';
+        },
+        style: {
+          color: '#ffffff',
+          fontSize: '16px',
+          fontWeight: '700'
+        },
+        y: 10
+      }
+    }],
 
-      series: [{
-        type: 'bar',
-        pointWidth: 25, // thicker bars (Asks)
-        dataLabels: [{
-          align: 'right',
-          alignTo: 'plotEdges',
-          style: {
-            fontSize: '14px',
-            textOutline: 'none'
-          },
-          format: '{point.y:,.0f}'
-        }, {
-          align: 'left',
-          inside: true,
-          style: {
-            fontSize: '13px',
-            textOutline: 'none'
-          },
-          format: '{point.price:,.5f}'
-        }],
-        name: 'Asks',
-        color: '#C1666B',
-        data: this.asksData
+    plotOptions: {
+      series: {
+        animation: false,
+        dataLabels: {
+          enabled: true,
+          color: '#ffffff'
+        },
+        borderWidth: 0,
+        crisp: false
+      }
+    },
+
+    series: [{
+      type: 'bar',
+      pointWidth: 25, // thicker bars (Asks)
+      dataLabels: [{
+        align: 'right',
+        alignTo: 'plotEdges',
+        style: {
+          fontSize: '14px',
+          textOutline: 'none'
+        },
+        format: '{point.y:,.0f}'
       }, {
-        type: 'bar',
-        pointWidth: 25, // thicker bars (Bids)
-        dataLabels: [{
-          align: 'left',
-          alignTo: 'plotEdges',
-          style: {
-            fontSize: '14px',
-            textOutline: 'none'
-          },
-          format: '{point.y:,.0f}'
-        }, {
-          align: 'right',
-          inside: true,
-          style: {
-            fontSize: '13px',
-            textOutline: 'none'
-          },
-          format: '{point.price:,.5f}'
-        }],
-        name: 'Bids',
-        color: '#48A9A6',
-        data: this.bidsData,
-        yAxis: 1
-      }]
-    };
+        align: 'left',
+        inside: true,
+        style: {
+          fontSize: '13px',
+          textOutline: 'none'
+        },
+        format: '{point.price:,.5f}'
+      }],
+      name: 'Asks',
+      color: '#C1666B',
+      data: this.asksData
+    }, {
+      type: 'bar',
+      pointWidth: 25, // thicker bars (Bids)
+      dataLabels: [{
+        align: 'left',
+        alignTo: 'plotEdges',
+        style: {
+          fontSize: '14px',
+          textOutline: 'none'
+        },
+        format: '{point.y:,.0f}'
+      }, {
+        align: 'right',
+        inside: true,
+        style: {
+          fontSize: '13px',
+          textOutline: 'none'
+        },
+        format: '{point.price:,.5f}'
+      }],
+      name: 'Bids',
+      color: '#48A9A6',
+      data: this.bidsData,
+      yAxis: 1
+    }]
+  };
 
   ngOnInit(): void {
     console.log('connecting...');
